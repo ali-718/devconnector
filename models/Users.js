cca3dba1415cd3e51eecd1c87784bb6b4bfcb987
ali-718 <alimurtuza718@gmail.com>
2019-05-21 05:01:30 -0700

    Login is fixed

--- models/Users.js
+++ models/Users.js
@@ -1,73 +1,70 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-    createxAt: {
-        type: Date,
-        default: Date.now
-    },
-})
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  createxAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-userSchema.pre("save", function (next) {
-    user = this;
+userSchema.pre("save", function(next) {
+  user = this;
 
-    if (user.isModified("password")) {
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(user.password, salt, (err, hash) => {
-                user.password = hash
-                next();
-            })
-        })
-    } else {
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
         next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({
+    email
+  }).then(user => {
+    if (!user) {
+      // return res.status(404).json({
+      //     error: "user not found"
+      // })
+      return Promise.reject({ err: "user not found" });
     }
-})
 
-userSchema.statics.findByCredentials = function (email, password) {
-    var User = this;
-
-    return User.findOne({
-        email
-    }).then(user => {
-        if (!user) {
-            // return res.status(404).json({
-            //     error: "user not found"
-            // })
-            return Promise.reject({
-                err: "user not found"
-            })
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject({ err: "incorrect password" });
         }
+      });
+    });
+  });
+};
 
-        return new Promise((resolve, reject) => {
-            bcrypt.compare(password, user.password, (err, res) => {
-                if (res) {
-                    resolve(user)
-                } else {
-                    reject(err)
-                }
-            })
-        })
+User = mongoose.model("users", userSchema);
 
-    })
-}
-
-User = mongoose.model('users', userSchema)
-
-module.exports = User
+module.exports = User;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,78 +1,90 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../../models/Users');
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-
+const User = require("../../models/Users");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // @route /api/users/
 // @desc testing routes
 // @status Public route
-router.get('/test', (req, res) => {
-    res.json({
-        message: "user route is working"
-    })
+router.get("/test", (req, res) => {
+  res.json({
+    message: "user route is working"
+  });
 });
 
 // @route /api/users/register
 // @desc Register route
 // @status Public route
-router.post('/register', (req, res) => {
-    User.findOne({
-        email: req.body.email
-    }).then(user => {
-        if (user) {
-            return res.json({
-                error: "Email already exist"
-            })
-        } else {
-            const avatar = gravatar.url(req.body.email, {
-                s: '200', //size
-                r: 'pg', //rating
-                d: 'mm' //default
-            })
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.json({
+        error: "Email already exist"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
 
-            const userSchema = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                avatar,
-            });
+      const userSchema = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
 
-            userSchema.save().then(user => res.json(user)).catch(err => res.json(err, "error occoured"))
-        }
-    })
+      userSchema
+        .save()
+        .then(user => res.json(user))
+        .catch(err => res.json(err, "error occoured"));
+    }
+  });
 });
-
 
 // @route /api/users/login
 // @desc Login th User / returning JWT
 // @status Public route
-router.post('/login', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    User.findOne({
-        email
-    }).then(user => {
-        if (!user) {
-            return res.status(404).json({
-                error: "user not found"
-            })
-        }
+  //   User.findOne({
+  //     email
+  //   }).then(user => {
+  //     if (!user) {
+  //       return res.status(404).json({
+  //         error: "user not found"
+  //       });
+  //     }
 
-        bcrypt.compare(password, user.password).then(pass => {
-            if (pass) {
-                res.json({
-                    msg: "succesfully login"
-                })
-            } else {
-                res.status(400).json({
-                    err: "password is incorrect"
-                })
-            }
-        })
+  //     bcrypt.compare(password, user.password).then(pass => {
+  //       if (pass) {
+  //         res.json({
+  //           msg: "succesfully login"
+  //         });
+  //       } else {
+  //         res.status(400).json({
+  //           err: "password is incorrect"
+  //         });
+  //       }
+  //     });
+  //   });
+
+  User.findByCredentials(email, password)
+    .then(user => {
+      if (!user) {
+        res.status(400).json({ err: "user not found" });
+      }
+      res.json({ user, msg: "successfully login" });
     })
-})
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
